21ebcb92aa024e571a65c208abf0324191f26b57
Arpita Roy <arpita.roy.workar@gmail.com>
2023-02-19 19:50:38 +0530

    Promise rejection practice

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        throw Error("I'm an error!")
-        // document.body.style.backgroundImage = `url(${data.urls.regular})`
-		// document.getElementById("author").textContent = `By: ${data.user.name}`
+        console.log(data.user.name)
+         console.log(data.urls.regular)
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        console.log("Something went wrong! 😭")
-        // This is where I can handle the error
-        // Choose to use a default background image
-        document.body.style.background = "rgb(68, 21, 68)"
+        /**
+         * Challenge: get a URL for a default background image and set it here
+         *
+         * 1. Change the query in the URL above back to something real
+         * 2. Log the image url to the console (replacing console.log(data) above)
+         * 3. Use that URL as the "default" background image to be used if
+         *    the promise is ever rejected.
+         */
+            document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1453872302360-eed3c5f8ff66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzY4MTQ2NzM&ixlib=rb-4.0.3&q=80&w=1080")`
+            document.getElementById("author").textContent = `By: Andrew Coelho`
     })
 
