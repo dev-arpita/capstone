7bd1f6c4d53b5873dde18ae3b655d35a3d04b882
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-03 10:40:41 +0530

    crypto- get dogecoin data

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        console.log(data.user.name)
-         console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        /**
-         * Challenge: get a URL for a default background image and set it here
-         *
-         * 1. Change the query in the URL above back to something real
-         * 2. Log the image url to the console (replacing console.log(data) above)
-         * 3. Use that URL as the "default" background image to be used if
-         *    the promise is ever rejected.
-         */
-            document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1453872302360-eed3c5f8ff66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzY4MTQ2NzM&ixlib=rb-4.0.3&q=80&w=1080")`
-            document.getElementById("author").textContent = `By: Andrew Coelho`
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
+/**
+ * Challenge: Pull down the cryptocurrency data for dogecoin from the
+ * CoinGecko API and log it to the console
+ *
+ * Also add a .catch() method to console any errors that might occur to the console
+ */
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
